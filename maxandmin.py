50c407974f5bc1a1e8460dd7f27c518be1a588aa
AVISHKAR-R <avishkar23r@gmail.com>
2020-06-02 18:10:28 +0530

    Add files via upload

---
+++ maxandmin.py
@@ -0,0 +1,7 @@
+num=int(input('Enter the decimal number:'))
+conv=input('Type number of conversion method \n\t 1.BIN \n\t 2.OCT \n\t 3.HEX\n')
+if conv=='1':n=2
+elif conv=='2': n=8
+elif conv=='3':n=16
+else:print('Enter Valid value') 
+val=0  
